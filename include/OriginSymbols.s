29672bf93183ec1508771a756446a8003ae4689f
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-22 17:08:25 +0800

    函数定义

--- include/OriginSymbols.s
+++ include/OriginSymbols.s
@@ -1,21 +1,49 @@
 TileToVram                              equ 0x08002F68
-PrintChapterAndGameSelectText          equ 0x08004E9C
+PrintChapterAndGameSelectText           equ 0x08004E9C
 PrintText                               equ 0x0800517C
 ChapterAndGameSelect                    equ 0x0800729C
 
+TextOfMenu                              equ 0x08125E88
+TextOfSubMenu                           equ 0x08125EC0
 TextOfChapterSelect                     equ 0x08137028
 TextOfMiniGameSelect                    equ 0x08137032
 TextOfCredit                            equ 0x0813DBEA
+OffsetOfTextOfCredit                    equ 0x0813DE42
+DataOfCardNameJpn                       equ 0x0814EFD0
+PtrOfTextOfCardNameJpn                  equ 0x0814F5A0
 TextOfMiniGame                          equ 0x08157764
 TextOfChapter                           equ 0x083B29E8
 
 LengthOfTextOfMiniGame                  equ 0x08157A4C
 
+
+GfxOfLogoComp                           equ 0x0810EC74
+GfxOfPushStart                          equ 0x0810FE10
+GfxOfMenuTopComp                        equ 0x08110798
+GfxOfFont16x16ForCardNameComp           equ 0x0814E04C
+MapOfStoryReviewComp                    equ 0x08112078
+MapOfStoryModeComp                      equ 0x08111F62
+
 GfxOfFont12x12ForText                   equ 0x0814A5AC
 PalOfFont12x12ForText                   equ 0x0814E02C
-GfxOfFont16x16ForCardNameComp           equ 0x0814E04C
 PtrOfFont12x12ForText1                  equ 0x0800706C
 PtrOfFont12x12ForText2                  equ 0x08007258
 PtrOfFont12x12ForText3                  equ 0x080094AC
 PtrOfFont12x12ForText4                  equ 0x0800CBA0
 PtrOfFont12x12ForText5                  equ 0x0800DC74
+
+GfxOfMenuFont                           equ 0x08121DEC
+GfxOfMenuTips                           equ 0x081257EC
+
+ObjOfMenuCursorStatic                   equ 0x081272BC
+PtrOfObjOfMenuCursorStatic              equ 0x08127448
+ObjOfMenuCursorDynamic                  equ 0x08127470
+PtrOfObjOfMenuCursorDynamic             equ 0x08127744
+
+GfxOfStartBackgroundComp                equ 0x08108A20
+GfxOfEndComp                            equ 0x08137076
+
+GfxOfPauseMenuComp                      equ 0x0813DEA4
+
+GfxOfCardBackComp                       equ 0x083B8230
+GfxOfCard51LoveComp                     equ 0x083D883E
