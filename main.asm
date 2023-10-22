21ff01653f10df6e1cdbadf1006c06a11875e80a
GitHub Wokann <Kenny_chen1998@outlook.com>
2023-10-22 17:09:07 +0800

    主文件

--- main.asm
+++ main.asm
@@ -7,21 +7,27 @@
 .include "./include/OriginSymbols.s"
 .include "./include/HackSymbols.s"
 
-.include "./data/ChapterDataPtr.s"
+;.include "./data/DataOfMenuCursor.s"
 .include "./data/LengthOfTextOfMiniGame.s"
+.include "./data/PtrOfTextOfCardName.s"
+.include "./data/PtrOfTextOfChapter.s"
+
+.include "./strings/TextOfCardName.s"
+.include "./strings/TextOfChapter.s"
+.include "./strings/TextOfCredit.s"
+.include "./strings/TextOfMiniGame.s"
+.include "./strings/TextOfMenu.s"
 
 .include "./src/HackInOrigin.s"
+.include "./graphic/graphicInOriginAdd.s"
 
 .org HackStart1
 .include "./src/CopyOrigin.s"
-.include "./src/CardName.s"
+.include "./src/DMA.s"
+.include "./src/SkipCompTileToUncompData.s"
 .include "./src/ExpandFont.s"
-.include "./graphic/fonts.s"
 
-.include "./strings/TextOfChapter.s"
-.include "./strings/TextOfMiniGame.s"
-
-;.include "./graphic/graphic.s"
+.include "./graphic/graphicInNewAdd.s"
 
 
 .close
